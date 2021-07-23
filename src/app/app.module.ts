e0d3afd1084c172a60807b876ba235dc6f84bd59
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-22 23:08:48 -0300

    Finalizado As solicitaçoes do Back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -19,7 +20,7 @@ import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, ReadAllComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatCardModule, MatButtonModule, MatIconModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -1,3 +1,4 @@
+import { TodoService } from './../../services/todo.service';
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
 
@@ -7,22 +8,18 @@ import { Todo } from 'src/app/models/todo';
   styleUrls: ['./read-all.component.css']
 })
 export class ReadAllComponent implements OnInit {
-  list: Todo[] = [
-    {
-      titulo: "Teste",
-      dataParaFinalizar: new Date,
-      finalizado: false
-    },
-    {
-      titulo: "Teste2",
-      dataParaFinalizar: new Date,
-      finalizado: false
-    }
-  ]
+  list: Todo[] = [ ];
 
-  constructor() { }
+  constructor(private service: TodoService) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) =>{
+      this.list = resposta;
+    })
+  }
 }
